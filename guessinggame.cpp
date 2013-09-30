365da57e521f95bdfb6e64c39d15aff1373114e0
Joe Gleeson <gleesonjw05@uww.edu>
2013-09-30 16:47:45 -0500

    Added multiple guess support.  Added README.txt.

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -5,19 +5,21 @@ int main() {
  srand(time(NULL));
  int number = rand() % 100 + 1;
  int guess;
+ int guesses = 0;
+while (guess != number) {
 
 cout << "Guess the number 1-100: ";
 cin >> guess;
 if (guess == number) {
         cout << "You got it!" << endl;
+        break;
 }
 else if (guess > number) {
-        cout << "You guessed too high." << endl;
+        cout << "You guessed too high. Try again." << endl;
 }
 else {
-        cout << "You guessed too low." << endl;
+        cout << "You guessed too low. Try again." << endl;
 }
-
+}
 return 0;
 }
-
